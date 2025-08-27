a029f4632779915e5ce7b3b23431c3ed22baf1e5
Moaad Amer <moaad02@gmail.com>
2025-08-27 15:18:05 +0300

    Sorting -> BubbleSort

--- CodeProblems/Program.cs
+++ CodeProblems/Program.cs
@@ -1,5 +1,52 @@
 ﻿
 
 using CodeProblems.Easy;
+using CodeProblems.NeetCode150;
+using CodeProblems.Sorting;
 
-Console.WriteLine(EasyProblems.ClearDigits("cb34"));
+//Console.WriteLine(EasyProblems.ClearDigits("cb34"));
+
+
+//var s = "MCMXCIV";
+
+//Console.WriteLine(EasyProblems.RomanToInt(s));
+
+
+//Console.WriteLine(EasyProblems.IsPalindrome("A man, a plan, a canal: Panama"));
+
+
+//string[] strs = ["cir", "car"];
+//Console.WriteLine(EasyProblems.LongestCommonPrefix(strs));
+
+
+//var l1 = new ArraysAndStrings.ListNode(1);
+//l1.next = new ArraysAndStrings.ListNode(2);
+//l1.next.next = new ArraysAndStrings.ListNode(3);
+
+//var l2 = new ArraysAndStrings.ListNode(1);
+//l2.next = new ArraysAndStrings.ListNode(3);
+//l2.next.next = new ArraysAndStrings.ListNode(4);
+//l2.next.next.next = new ArraysAndStrings.ListNode(5);
+
+//var l3 = ArraysAndStrings.MergeTwoLists(l1, l2);
+
+//Console.WriteLine();
+
+
+//var map = new HashMap();
+
+//Console.WriteLine(map.CanConstruct("aa", "aab"));
+
+
+//ArraysAndHashing arraysAndHashing = new ArraysAndHashing();
+////arraysAndHashing.GroupAnagrams2(["bdddddddddd", "bbbbbbbbbbc"]);
+
+//arraysAndHashing.TopKFrequent([1, 2, 1, 2, 1, 2, 3, 1, 3, 2], 2);
+
+Sorting sorting = new Sorting();
+int[] nums = [-5, 3, 2,1,-3,-3,7,2,2];
+sorting.BubbleSort(nums);
+foreach(int num in nums)
+{
+    Console.WriteLine(num);
+}
---
+++ CodeProblems/Sorting/Sorting.cs
@@ -0,0 +1,31 @@
+﻿namespace CodeProblems.Sorting
+{
+    public class Sorting
+    {
+
+        //Runtime O(n^2)
+        //Spacetime O(1)
+        public void BubbleSort(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool swap = false;
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[j - 1] > nums[j])
+                    {
+                        int temp = nums[j - 1];
+                        nums[j - 1] = nums[j];
+                        nums[j] = temp;
+                        swap = true;
+                    }
+                }
+                if (!swap)
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+}
