66622fbda242781dc13e248656b6e1e47b6a6a48
Moaad Amer <moaad02@gmail.com>
2025-08-29 15:25:49 +0300

    3. Longest Substring Without Repeating Characters

--- CodeProblems/NeetCode150/SlidingWindow.cs
+++ CodeProblems/NeetCode150/SlidingWindow.cs
@@ -57,5 +57,65 @@
             }
             return max;
         }
+
+        //3. Longest Substring Without Repeating Characters
+        //Medium
+
+        //Given a string s, find the length of the longest substring without duplicate characters.
+
+        //Example 1:
+
+        //Input: s = "abcabcbb"
+        //Output: 3
+        //Explanation: The answer is "abc", with the length of 3.
+        //Example 2:
+
+        //Input: s = "bbbbb"
+        //Output: 1
+        //Explanation: The answer is "b", with the length of 1.
+        //Example 3:
+
+        //Input: s = "pwwkew"
+        //Output: 3
+        //Explanation: The answer is "wke", with the length of 3.
+        //Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+        //Constraints:
+
+        //0 <= s.length <= 5 * 104
+        //s consists of English letters, digits, symbols and spaces.
+
+        //Solution 1
+        //Runtime O(N)
+        //Space O(N)
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length < 2)
+            {
+                return s.Length;
+            }
+            int l = 0;
+            int r = 0;
+            int max = 0;
+            HashSet<char> chars = new HashSet<char>();
+            while (r < s.Length)
+            {
+                while (chars.Contains(s[r]))
+                {
+                    chars.Remove(s[l]);
+                    l++;
+                }
+                chars.Add(s[r]);
+                if (chars.Count > max)
+                {
+                    max = chars.Count;
+                }
+                r++;
+            }
+            return max;
+        }
+
     }
 }
